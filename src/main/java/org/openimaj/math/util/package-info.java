d5d307cb01fb48a697a2bd80d8129bb35127653f
HullBend <HullBend@users.noreply.github.com>
2018-11-25 15:51:50 +0100

    cosmetics

--- src/main/java/org/openimaj/math/util/package-info.java
+++ src/main/java/org/openimaj/math/util/package-info.java
@@ -1,5 +1,5 @@
 /**
- * A few utilities from OpenIMAJ's core-math module.
+ * Some utilities from OpenIMAJ's core-math module.
  * <p>
  * https://github.com/openimaj/openimaj/tree/master/core/core-math/src/main/java/org/openimaj/math
  */
